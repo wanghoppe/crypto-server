b82e74f9ce9b991bd7c8e0a7863a8a4967fde755
hoppe <wanghp000@gmail.com>
2020-01-15 02:23:59 +0800

    test3

--- realtime_pipeline.py
+++ realtime_pipeline.py
@@ -53,13 +53,13 @@ def get_ticker_social(df,coin_social):
     return complete_df
 
 def time_series_to_supervised(df,sequence_length):
-    
+
     '''for real time pipeline, we don't need label, i.e. actual y, so when we do indexing
     , we use "index+1 : index + sequence"
     '''
     temp = df.values
     temp = temp.tolist()
-        
+
     result = []
     for index in range(len(temp) - sequence_length +1):
         result.append(temp[index +1: index + sequence_length])
@@ -67,42 +67,42 @@ def time_series_to_supervised(df,sequence_length):
 
     # get x and y
     x = data
-        
+
     return x
 
 def data_preparation(coin, coinID, time_period, time_frequency, agg, apiKey, cat, lang, ticker_num, sequence_length):
-    
+
 
     def get_news_data_spec(cat, timestamp, apiKey, lang):
-        news_url = "https://min-api.cryptocompare.com/data/v2/news/?categories={}&lang={}&lTs={}&api_key={}".format(cat, lang, timestamp ,apiKey)        
+        news_url = "https://min-api.cryptocompare.com/data/v2/news/?categories={}&lang={}&lTs={}&api_key={}".format(cat, lang, timestamp ,apiKey)
         r = requests.get(news_url)
         ipdata = r.json()
         return ipdata
 
     # get current time
     timestamp = int(time.time())
-    
+
     prediction_length = ticker_num + sequence_length -1
-    
+
     # APIs
     price_url = "https://min-api.cryptocompare.com/data/{}?fsym={}&tsym=USD&limit={}&toTs={}&api_key={}".format(time_period, coin, prediction_length, timestamp,apiKey)
-    
+
     social_url = "https://min-api.cryptocompare.com/data/social/coin/histo/{}?coinId={}&aggregate={}&limit={}&toTs={}&api_key={}"\
         .format(time_frequency, coinID, agg, prediction_length, timestamp ,apiKey)
-    
+
 
     # get data
     price = requests.get(price_url).json()
     social = requests.get(social_url).json()
-    
+
     # deal with price
     price_df = pd.DataFrame(price['Data'])
     price_benchmark = price_df['close'][sequence_length-1]
     timestamp_benchmark = price_df['time'][sequence_length-1:]
-    price_df['date/hour'] = pd.to_datetime(price_df['time'], unit='s') 
+    price_df['date/hour'] = pd.to_datetime(price_df['time'], unit='s')
     price_df.set_index('date/hour', inplace=True)
     return_df = price_to_return(price_df,'close')
-    
+
     # deal with news
     news_ts = timestamp
     from_t = news_ts - (prediction_length * 3600) - 86400
@@ -111,7 +111,7 @@ def data_preparation(coin, coinID, time_period, time_frequency, agg, apiKey, cat
         news_data = get_news_data_spec(cat, news_ts, apiKey, lang)
         news_df = pd.DataFrame(news_data['Data'])
         holder.append(news_df)
-        news_ts = news_df['published_on'].min()        
+        news_ts = news_df['published_on'].min()
     df = pd.concat(holder, axis = 0)
     df = df[df['published_on']>from_t]
     df['time'] = df['published_on'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime("%Y-%m-%d"))
@@ -125,59 +125,59 @@ def data_preparation(coin, coinID, time_period, time_frequency, agg, apiKey, cat
     news['vader_pos'] = news['vader_polarity'].apply(lambda x: x['pos'])
     news = news[['id','published_on','vader_compound','vader_neg','vader_neu','vader_pos']]
     return_news_df = get_ticker_sentiment(return_df, news)
-    
+
     # deal with social
     social_df = pd.DataFrame(social['Data'])
     social_df.set_index('time',inplace = True)
     coin_complete = get_ticker_social(return_news_df,social_df)
-    
+
     # complete df
     coin_complete['time'] = pd.to_datetime(coin_complete['time'], unit='s')
     coin_complete['time'] = coin_complete['time'].dt.strftime('%Y-%m-%d %r')
     coin_complete.set_index('time', inplace=True)
-    
+
     return price_benchmark, timestamp_benchmark, coin_complete
 
 def pipeline(coin_complete, sequence_length, model):
-    
+
     coin_complete = coin_complete.replace([np.inf, -np.inf], np.nan)
     coin_complete = coin_complete.fillna(0)
-    
+
     # scale data
     max_abs_scaler = MaxAbsScaler()
     temp = max_abs_scaler.fit_transform(coin_complete)
     temp = pd.DataFrame(temp,columns=coin_complete.columns)
     coin_complete = temp.set_index(coin_complete.index)
-    
+
     x = time_series_to_supervised(coin_complete, sequence_length)
-    
+
     y_predict = model.predict(x)
-    
+
     # inverse scaler
     y_predict_inverse = y_predict * max_abs_scaler.scale_[1]
-    
+
     return y_predict, y_predict_inverse
 
 def denormalized_to_actual_price(y_predict_inverse, price_benchmark, timestamp_benchmark):
-    
+
     # log return to simple return
     predict_simple = np.exp(y_predict_inverse)
     #actual_simple = np.exp(y_actual_inverse)
-    
+
     # get real price
     predict_simple = np.insert(predict_simple, 0, price_benchmark)
     predict_price = predict_simple.cumprod()[1:]
     #actual_simple = np.insert(actual_simple, 0, price_benchmark)
     #actual_price = actual_simple.cumprod()[1:]
-    
+
     # get timestamp
     timestamp_benchmark = timestamp_benchmark[1:]
     timestamp_benchmark = timestamp_benchmark + 3600
-    
+
     # zip
     predict_price = list(zip(timestamp_benchmark, predict_price))
     #actual_price = list(zip(timestamp_benchmark, actual_price))
-    
+
     return predict_price
 
 if __name__ == '__main__':
@@ -185,7 +185,7 @@ if __name__ == '__main__':
     # credentials
     cred = pika.credentials.PlainCredentials('hwa125', '960923')
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='ec2-3-19-67-238.us-east-2.compute.amazonaws.com', credentials=cred))
+        pika.ConnectionParameters(host='localhost', credentials=cred))
     channel = connection.channel()
 
     channel.queue_declare(queue='predict-24')
@@ -213,7 +213,7 @@ if __name__ == '__main__':
         # get coin data
         price_benchmark, timestamp_benchmark, coin_complete = data_preparation(
             coin, coinID, timeperiod, timefrequency, agg, apiKey, cat, lang, ticker_num, sequence_length)
-        
+
         print ('start to get prediction price')
         # get prediction
         y_predict, y_predict_inverse = pipeline(coin_complete, sequence_length, coin_model)
@@ -227,7 +227,7 @@ if __name__ == '__main__':
         print ('--------------publish---------------')
         # publish data
         channel.basic_publish(exchange='', routing_key='predict-24', body=json.dumps(predict_price))
-        
+
         # sleep
         latest_ts = timestamp_benchmark[ticker_num+sequence_length-1]
         print ('latest timestamp is', latest_ts)
@@ -236,4 +236,3 @@ if __name__ == '__main__':
         wait_second = 3600 - (ts_now - latest_ts)
         print ('wait second is', wait_second)
         time.sleep(wait_second + 120)
-
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require("http");
 var amqp = require('amqplib/callback_api');
 const uuidv1 = require('uuid/v1');
 var response_map = new Map();
+var response_map2 = new Map();
+var stored_lst;
 // var second_object;
 
 function res_func2 (data_msg, response) {
@@ -26,6 +28,22 @@ amqp.connect('amqp://hwa125:960923@localhost', function(err, conn) {
   });
 });
 
+amqp.connect('amqp://hwa125:960923@localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'predict-24';
+
+    ch.assertQueue(q, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+
+      let data_msg = msg.content.toString()
+      stored_lst = data_msg;
+      response_map2.forEach((res) => res_func2(stored_lst, res));
+
+    }, {noAck: true});
+  });
+});
+
 http
   .createServer((request, response)  => {
     console.log("Requested url: " + request.url);
@@ -44,6 +62,39 @@ http
         response_map.delete(uuid)
       })
     }
+
+    if (request.url.toLowerCase() === "/get_store") {
+      response.writeHead(200, {
+        // Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*"
+      });
+      console.log('here')
+      if ( stored_lst != "undefined") {
+        console.log('here222')
+        response.write(stored_lst)
+        response.write("\n\n")
+
+      }
+      response.end()
+    }
+
+    if (request.url.toLowerCase() === "/events") {
+      const uuid = uuidv1();
+      response.writeHead(200, {
+        Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*"
+      });
+      response_map2.set(uuid, response);
+      response.on('close', ()=>{
+        console.log(uuid);
+        response_map2.delete(uuid)
+      })
+    }
+
   })
   .listen(80, () => {
     console.log("Server running at http://127.0.0.1:80/");
